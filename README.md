ed3eb4e4cdb5b6055a87774c81c805e2ca6c4f0d
rubens-fs <rubens.frota@gmail.com>
2023-02-01 11:13:23 -0300

    Uploading the first version of the extension.

---
+++ README.md
@@ -0,0 +1,52 @@
+# README.md
+
+![icon-128px](imgs/icon-128.png)
+
+## 📌 Introduction
+
+This is an extension for web browsers that allows for searching for free images in one place. Simply click on the desired option and type in a search term. 
+
+The user can click or simply hit `ENTER` to perform the search and the corresponding images will be presented. 
+
+It is important to note that all sites are in your English version, so it is recommended to make searches in English as well. 
+
+The tools used were:
+Javascript for all the code's functionality, CSS for styling, and HTML for the structure and organization of the extension's elements.
+
+In addition to them, it has a .json file for the necessary specifications for the extension to work perfectly.
+
+## 👨‍💻 About the interface
+
+This code is a browser extension designed for finding free images. It features an HTML form including checkboxes for selecting different image search websites such as Unsplash, Pexels, Pixabay, and more, as well as a search bar.
+
+The added JavaScript manages the selection of options and the search, opening the corresponding pages of the selected websites with the corresponding search. It also includes verification to ensure that the user has selected an option and entered a search term before performing the search.
+
+## 💻 Midia
+
+### Interface
+
+![home-page](imgs/home.png)
+
+### Its functioning:
+
+![Interface on Google Chrome](/imgs/demo-google-chrome.gif)
+
+Interface on Google Chrome.
+
+![Interface on Microsoft Edge](/imgs/demo-edge.gif)
+
+Interface on Microsoft Edge.
+
+`!important` The interface was also tested (successfully) on the Opera GX browser. ✔️
+
+## ⚙ How to add?
+
+Go to chrome://extensions/. At the top right, turn on Developer mode. Click Load unpacked. Find and select the app or extension folder.
+
+In other browsers the procedure is similar.
+
+### ❤️ I hope you like it!
+
+This was my final project for the CS50 course offered by Harvard, it was really challenging and quite rewarding for me to create it!
+
+By the way, here is my LinkedIn profile! https://www.linkedin.com/in/rubens-fs/ (If you would like to connect with me, feel free! 😃).
---
+++ js/popup.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // JS will only run after the HTML is loaded.
+  let selectedOption = null;
+
+  function chosenOption(option) {
+    // Uncheck the previous option
+    if (selectedOption) {
+      document.getElementById(selectedOption + '-search').checked = false;
+    }
+    // Mark the new option
+    document.getElementById(option + '-search').checked = true;
+    selectedOption = option;
+  }
+
+  let options = document.querySelectorAll('.search-option');
+  
+  for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', function() {
+      chosenOption(this.id.split('-')[0]);
+    });
+  }
+
+  const searchButton = document.querySelector("#btn");
+
+  // If you want to add more websites for search, besides adding them below and in the HTML file, pay attention to the total size of the extension (CSS)
+  function search() {
+    let searchInput = document.querySelector('#search-bar');
+    if (!searchInput.value) {
+      return alert('Please make sure you have an option selected and/or a search term.');
+    }
+    if (selectedOption === 'unsplash') {
+      window.open(
+        'https://unsplash.com/s/photos/' + searchInput.value, '_blank'
+      )
+    } else if (selectedOption === 'pexels') {
+      window.open(
+        'https://www.pexels.com/search/' + searchInput.value, '_blank'
+      )
+    } else if (selectedOption === 'pixabay') {
+      window.open(
+        'https://pixabay.com/images/search/' + searchInput.value, '_blank'
+      )
+    } else if (selectedOption === 'stocksnap') {
+      window.open(
+        'https://stocksnap.io/search/' + searchInput.value, '_blank'
+      )
+    } else if (selectedOption === 'freeimages') {
+      window.open(
+        'https://www.freeimages.com/search/' + searchInput.value, '_blank'
+      )
+    } else if (selectedOption === 'negativespace') {
+      window.open(
+        'https://negativespace.co/?s=' + searchInput.value, '_blank'
+      )
+    } else {
+      return alert('Please select a search option!');
+    }
+  }
+
+  // Active the function when ENTER is pressed
+  document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+      let btn_enter = document.querySelector("#btn");
+      btn_enter.click();
+    }
+  });
+
+  searchButton.addEventListener("click", search);
+});
