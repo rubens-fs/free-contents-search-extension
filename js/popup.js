c6fb9105ea78b4f013aa1347346e93a89f1bbb5e
rubens-fs <rubens.frota@gmail.com>
2023-03-21 11:21:10 -0300

    Features for switch button and videos section

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   // JS will only run after the HTML is loaded.
+  const switchBtn = document.querySelector('.switch input');
+  const title = document.querySelector('#title');
+
+  function swap() {
+    if (this.checked) {
+      title.textContent = 'Free Videos Search';
+      document.querySelector('.options-videos').style.display = 'block';
+      document.querySelector('.options-photos').style.display = 'none';
+    } else {
+      title.textContent = 'Free Images Search';
+      document.querySelector('.options-photos').style.display = 'block';
+      document.querySelector('.options-videos').style.display = 'none';
+    }
+  };
+
   let selectedOption = null;
 
   function chosenOption(option) {
@@ -29,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
       return alert('Please make sure you have an option selected and/or a search term.');
     }
     switch (selectedOption) {
+      /* Photos section */
       case 'unsplash':
           window.open (
               'https://unsplash.com/s/photos/' + searchInput.value, '_blank'
@@ -59,9 +75,40 @@ document.addEventListener("DOMContentLoaded", function() {
               'https://negativespace.co/?s=' + searchInput.value, '_blank'
           );
           break;
+      /* Videos section */
+      case 'videvo':
+          window.open (
+              'https://www.videvo.net/search/' + searchInput.value, '_blank'
+          );
+          break;
+      case 'pexelsVideos':
+          window.open (
+              'https://www.pexels.com/search/videos/' + searchInput.value, '_blank'
+          );
+          break;
+      case 'pixabayVideos':
+          window.open (
+              'https://pixabay.com/videos/search/' + searchInput.value, '_blank'
+          );
+          break;
+      case 'mazwai':
+          window.open (
+              'https://mazwai.com/stock-video-footage/' + searchInput.value, '_blank'
+          );
+          break;
+      case 'coverr':
+          window.open (
+              'https://coverr.co/s?q=' + searchInput.value, '_blank'
+          );
+          break;
+      case 'videezy':
+          window.open (
+              'https://www.videezy.com/free-video/' + searchInput.value, '_blank'
+          );
+          break;
       default:
           alert('Please select a search option!');
-      }
+    }
   }
 
   // Active the function when ENTER is pressed
@@ -72,5 +119,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+  switchBtn.addEventListener("change", swap);
   searchButton.addEventListener("click", search);
 });
